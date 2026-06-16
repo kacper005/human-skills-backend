8d285d86231870f640552749757e3fb6d4bc0f6d
Maŧhias <mathiawj@stud.ntnu.no>
2026-06-16 13:08:45 +0200

    Created Game session and Game template entities

---
+++ src/main/java/com/talection/talection/model/gamepersistance/GameSession.java
@@ -0,0 +1,119 @@
+package com.talection.talection.model.gamepersistance;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+public class GameSession {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  private Long userId;
+
+  @NotNull
+  private Long gameTemplateId;
+
+  @NotNull
+  private Date startedAt;
+
+  @NotNull
+  private Date completedAt;
+
+  @NotNull
+  private Integer score;
+
+  // Getters and setters
+
+  /**
+   * Gets the ID of the game session.
+   * 
+   * @return the ID of the game session
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Gets the ID of the user associated with the game session.
+   * 
+   * @return the ID of the user
+   */
+  public Long getUserId() {
+    return userId;
+  }
+
+  /**
+   * Gets the start date of the game session.
+   * 
+   * @return the start date of the game session
+   */
+  public Date getStartedAt() {
+    return startedAt;
+  }
+
+  /**
+   * Gets the completion date of the game session.
+   * 
+   * @return the completion date of the game session
+   */
+  public Date getCompletedAt() {
+    return completedAt;
+  }
+
+  /**
+   * Gets the score of the game session.
+   * 
+   * @return the score of the game session
+   */
+  public Integer getScore() {
+    return score;
+  }
+
+  /**
+   * Sets the ID of the game session.
+   * 
+   * @param id the ID of the game session
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * Sets the ID of the user associated with the game session.
+   * 
+   * @param userId the ID of the user
+   */
+  public void setUserId(Long userId) {
+    this.userId = userId;
+  }
+
+  /**
+   * Sets the start date of the game session.
+   * 
+   * @param startedAt the start date of the game session
+   */
+  public void setStartedAt(Date startedAt) {
+    this.startedAt = startedAt;
+  }
+
+  /**
+   * Sets the completion date of the game session.
+   * 
+   * @param completedAt the completion date of the game session
+   */
+  public void setCompletedAt(Date completedAt) {
+    this.completedAt = completedAt;
+  }
+
+  /**
+   * Sets the score of the game session.
+   * 
+   * @param score the score of the game session
+   */
+  public void setScore(Integer score) {
+    this.score = score;
+  }
+}
---
+++ src/main/java/com/talection/talection/model/games/GameTemplate.java
@@ -0,0 +1,124 @@
+package com.talection.talection.model.games;
+
+import com.talection.talection.enums.GameType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class GameTemplate {
+  @Id
+  @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  private String name;
+
+  private String description;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private GameType gameType;
+
+  @NotNull 
+  private Boolean active = true;
+
+  /**
+   * Default constructor for JPA.
+   * This constructor is required for JPA to create instances of this entity.
+   */
+  public GameTemplate() {
+    // Default constructor for JPA
+  }
+
+  /**
+   * Sets the ID of the game template.
+   * 
+   * @param id the ID to set for the game template
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * Set the name of the game template.
+   * 
+   * @param name the name to set for the game template
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Sets the description of the game template.
+   * 
+   * @param description the description to set for the game template
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  /**
+   * Set the game type of the game template.
+   * 
+   * @param gameType the game type to set for the game template
+    *
+   */
+  public void setGameType(GameType gameType) {
+    this.gameType = gameType;
+  }
+
+  /**
+   * Set the active status of the game template.
+   * 
+   * @param active the active status to set for the game template
+   */
+  public void setActive(Boolean active) {
+    this.active = active;
+  }
+
+  /**
+   * Gets the ID of the game template.
+   * 
+   * @return the ID of the game template
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Gets the name of the game template.
+   * 
+   * @return the name of the game template
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Gets the description of the game template.
+   * 
+   * @return the description of the game template
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+  * Gets the game type of the game template.
+  * 
+  * @return the game type of the game template
+  */
+  public GameType getGameType() {
+    return gameType;
+  }
+  
+  /**
+   * Gets the active status of the game template.
+   * 
+   * @return the active status of the game template
+   */
+  public Boolean getActive() {
+    return active;
+  }
+}
