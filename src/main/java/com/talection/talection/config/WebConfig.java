4e2cf290ec904f82a1408abe9679df7dac483378
Maŧhias <mathiawj@stud.ntnu.no>
2026-06-24 10:56:20 +0200

    Removed @EnableWebMvc which disables Boot's auto-configuration

--- src/main/java/com/talection/talection/config/WebConfig.java
+++ src/main/java/com/talection/talection/config/WebConfig.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Configuration class for CORS settings.
  */
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed.origins}")
